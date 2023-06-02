9de63922b9e370858ee84e95ddd8b8f5e97e8695
Henry Gordon <mrnumbersvt2012@gmail.com>
2023-06-02 16:25:13 -0400

    hjjj.
    jjhh.

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "a prestige tree mod",
+	id: "iamahenry",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
